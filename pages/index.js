38f7ded75090770653d83fefab8f767440b4b1db
marc <marc@delacruz.com>
2022-07-01 00:33:54 +0800

    Refactored home page

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,23 @@
 import Link from "next/link";
+import projects from "../definitions/projects";
 
 export default function Home() {
+  const projectLinks = projects.map((project) => (
+    <li className="">
+      <Link href={project.url} passHref>
+        <a className="text-sm whitespace-nowrap">{project.text}</a>
+      </Link>
+    </li>
+  ));
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-      <div className="font-Rubik font-light text-2xl sm:text-7xl flex">
+    <div className="flex flex-col h-screen items-center justify-center overflow-hidden">
+      <div className="font-Rubik font-light text-2xl sm:text-5xl md:text-7xl flex px-8">
         <div className="animate-pulse mx-2 font-bold">50</div> Projects in
         <div className="animate-bounce mx-2 font-bold">50</div> Days
       </div>
-      <div className="font-Karla font-light text-lg sm:text-xl pt-10">
-        <ul className="list-decimal flex space-x-10">
-          <li>
-            <Link href="/expanding-cards">Expanding Cards</Link>
-          </li>
-          <li>
-            <Link href="/progress-steps">Progress Steps</Link>
-          </li>
-          <li>
-            <Link href="/rotating-navigation">Rotating Navigation</Link>
-          </li>
-          <li>
-            <Link href="/hidden-search">Hidden Search</Link>
-          </li>
-          <li>
-            <Link href="/blurry-loading">Blurry Loading</Link>
-          </li>
-          <li>
-            <Link href="/scroll-animation">Scroll Animation</Link>
-          </li>
-          <li>
-            <Link href="/split-landing-page">Split Landing Page</Link>
-          </li>
-          <li>
-            <Link href="/form-input-wave">Form Input Wave</Link>
-          </li>
-          <li>
-            <Link href="/sound-board">Sound Board</Link>
-          </li>
+      <div className="font-Karla font-light pt-10 px-40 ">
+        <ul className="list-decimal grid grid-cols-1 lg:grid-cols-5 lg:gap-x-14 text-sm sm:text-xl">
+          {projectLinks}
         </ul>
       </div>
     </div>
